2add8366c9279a8d95b22838e8460ec2575a7fb5
Richard Stelling <richard.stelling@naimaudio.com>
2016-02-23 13:47:44 +0000

    Added SSEKit project. (second crack)

---
+++ Projects/SSEKit/SSEKit/SSEKit.h
@@ -0,0 +1,19 @@
+//
+//  SSEKit.h
+//  SSEKit
+//
+//  Created by Richard Stelling on 23/02/2016.
+//  Copyright © 2016 Naim Audio Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SSEKit.
+FOUNDATION_EXPORT double SSEKitVersionNumber;
+
+//! Project version string for SSEKit.
+FOUNDATION_EXPORT const unsigned char SSEKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SSEKit/PublicHeader.h>
+
+
