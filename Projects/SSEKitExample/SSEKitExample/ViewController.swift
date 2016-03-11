64e4858d6e475e432b3d627e32ac74f2aee36f8a
Richard Stelling <richard.stelling@naimaudio.com>
2016-03-11 14:26:35 +0000

    Updated Example app.

--- Projects/SSEKitExample/SSEKitExample/ViewController.swift
+++ Projects/SSEKitExample/SSEKitExample/ViewController.swift
@@ -3,23 +3,52 @@
 //  SSEKitExample
 //
 //  Created by Richard Stelling on 23/02/2016.
-//  Copyright © 2016 Naim Audio Ltd. All rights reserved.
+//  Copyright © 2016 Richard Stelling All rights reserved.
 //
 
 import UIKit
+import SSEKit
 
 class ViewController: UIViewController {
 
+    var manager: SSEManager?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        NSNotificationCenter.defaultCenter().addObserverForName(nil, object: nil, queue: nil) {
+            
+            //SSEManager.Notification.Key.Name.rawValue
+            
+            guard let name = $0.userInfo?[SSEManager.Notification.Key.Name.rawValue], let data = $0.userInfo?[SSEManager.Notification.Key.Data.rawValue] as? NSData else {
+                
+                return
+            }
+            
+            let dataStr = String(data: data, encoding: 4)!
+            
+            print("NOTE: \(name) -> \(dataStr)")
+        }
+        
+        
+        let config = EventSourceConfiguration(withHost: "localhost", port: 8080, endpoint: "/sse", events: ["user-connected"])
+        let config2 = EventSourceConfiguration(withHost: "localhost", port: 8080, endpoint: "/sse", events: nil)
+        //let config4 = EventSourceConfiguration(withHost: "192.168.37.76", port: 8080, endpoint: "/sse", events: ["bad-event"])
+        
+        //let config2 = EventSourceConfiguration(withHost: "localhost", port: 8080, endpoint: "/sse2")
+        //let config3 = EventSourceConfiguration(withHost: "localhost", port: 8081, endpoint: "/sse")
+
+        manager = SSEManager(sources: [config, config2])
+        //manager = SSEManager(sources: [config2])
+        //manager = SSEManager(sources: [config3])
+        
+        
+        //manager = SSEManager(sources: [config, config2, config3])
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
