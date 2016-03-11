4211ad439df037a4820a7fed2b3a7344f8ae780a
Richard Stelling <richard.stelling@naimaudio.com>
2016-03-11 14:18:00 +0000

    Created simple project for development and demoing.

---
+++ Projects/SSEKitExample/SSEKitExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SSEKitExample
+//
+//  Created by Richard Stelling on 23/02/2016.
+//  Copyright © 2016 Naim Audio Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
