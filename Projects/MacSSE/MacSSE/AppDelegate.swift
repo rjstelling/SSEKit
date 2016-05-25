fcc2be462725a5107c8c721b23e34e70cba28300
Richard Stelling <richard.stelling@naimaudio.com>
2016-05-25 09:55:16 +0100

    Added Mac app project.

---
+++ Projects/MacSSE/MacSSE/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MacSSE
+//
+//  Created by Richard Stelling on 23/05/2016.
+//  Copyright © 2016 Richard Stelling. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
